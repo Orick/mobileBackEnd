47f6ad69f10e0c5a93c1bd285dd9fae83df55c39
Alejandro Zuñiga <alejandro.zuniga@mail.udp.cl>
2018-09-23 13:07:31 -0300

    identa3
    cambio menor

--- routes/planta.js
+++ routes/planta.js
@@ -9,24 +9,24 @@ const Op = Sequelize.Op;
 app.use(bodyParser.urlencoded({ extended: false }));
 
 router.post('/insert',(req,res,next)=>{
-models.planta.create({
-    tipoPlanta: req.body.tipoPlanta,
-    humedadOptima: req.body.humedadOptima,
-    humedadMinima: req.body.humedadMinima,
-    luzOptima: req.body.luzOptima,
-    luzMinima: req.body.luzMinima
-}).then(maceteroCreated =>{
-    res.json({
-        status: 1,
-        statusCode: 'plantas/insert/ok',
-        description: 'ok',
-    });
-}).catch(error => {
-    res.json({
-        status: 0,
-        statusCode: 'planta/insert/error',
-        description: 'Error base de datos'
-    });
-});
+	models.planta.create({
+	    tipoPlanta: req.body.tipoPlanta,
+	    humedadOptima: req.body.humedadOptima,
+	    humedadMinima: req.body.humedadMinima,
+	    luzOptima: req.body.luzOptima,
+	    luzMinima: req.body.luzMinima
+	}).then(maceteroCreated =>{
+	    res.json({
+	        status: 1,
+	        statusCode: 'plantas/insert/ok',
+	        description: 'ok',
+	    });
+	}).catch(error => {
+	    res.json({
+	        status: 0,
+	        statusCode: 'planta/insert/error',
+	        description: 'Error base de datos'
+	    });
+	});
 });
     module.exports = router;
